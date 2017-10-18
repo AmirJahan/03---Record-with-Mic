91db685a2682b39e5e2df0d42f1c4cd6f0962666
Amir J <amir.jahan@gmail.com>
2017-10-18 12:35:01 -0700

    Initial Commit

---
+++ Record with Mic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Record with Mic
+//
+//  Created by Amir on 10/18/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Record with Mic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Record with Mic
+//
+//  Created by Amir on 10/18/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
